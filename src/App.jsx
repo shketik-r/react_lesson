eaa5c345e77bdb636b3af3cd207b03d7822db1c4
Ruslan <rysel3101@gmail.com>
2022-06-23 19:11:24 +0300

    wer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import './App.css';
-import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header';
 import Sandbar from './components/Sandbar/Sandbar';
+import Header from './components/Header/Header';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 import Footer from './components/Footer/Footer';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className='app-wrapper'>
-    <Header />
-    <Sandbar />
-    <Profile/>
-    <Footer />
-  </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Sandbar />
+        <section className='app_wrapper_content'>
+          <Routes>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/dialogs" element={<Dialogs />} />
+          </Routes>
+        </section>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
